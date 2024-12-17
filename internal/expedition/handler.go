13f3d389b8563a7b8e3efe83df6d96d970c37424
shoken <shoken4797@gmail.com>
2024-12-17 17:01:16 +0900

    遠征記録作成リクエスト型作成

--- internal/expedition/handler.go
+++ internal/expedition/handler.go
@@ -10,6 +10,7 @@ type ExpeditionHandler struct{}
 // @Description 遠征、出費、試合、訪れた施設の情報を保存する。
 // @Tags expedition
 // @Security BearerAuth
+// @Param request body CreateExpeditionRequest true "遠征記録作成リクエスト"
 // @Success 200 {object} utils.BasicResponse "アップロードした画像のURL"
 // @Failure 400 {object} utils.BasicResponse "リクエストエラー"
 // @Failure 403 {object} utils.BasicResponse "ユーザーが見つかりません"
@@ -32,7 +33,6 @@ func (h *ExpeditionHandler) CreateExpedition(c *gin.Context) {
 // @Router /api/expedition/update/{id} [put]
 func (h *ExpeditionHandler) UpdateExpedition(c *gin.Context) {
 }
-
 
 // @Summary 遠征記録を削除
 // @Description pathのidをもとに遠征記録を削除する。
--- internal/expedition/model.go
+++ internal/expedition/model.go
@@ -1,1 +1,44 @@
 package expedition
+
+import "time"
+
+// リクエスト
+type PaymentRequest struct {
+	Title string    `json:"title" binding:"required"`
+	Date  time.Time `json:"date" binding:"required"`
+	Cost  int       `json:"cost" binding:"required"`
+}
+
+type GameRequest struct {
+	Date      time.Time          `json:"date" binding:"required"`
+	Comment   string             `json:"comment" binding:"required"`
+	StadiumId uint               `json:"stadiumId" binding:"required"`
+	Team1Id   uint               `json:"team1Id" binding:"required"`
+	Team2Id   uint               `json:"team2Id" binding:"required"`
+	Scores    []GameScoreRequest `json:"scores" binding:"required"`
+}
+
+type GameScoreRequest struct {
+	Order  int  `json:"order" binding:"required"`
+	Score  int  `json:"score" binding:"required"`
+	TeamId uint `json:"teamId" binding:"required"`
+}
+
+type VisitedFacilityRequest struct {
+	Name      string  `json:"name" binding:"required"`
+	Address   string  `json:"address" binding:"required"`
+	Latitude  float64 `json:"latitude" binding:"required"`
+	Longitude float64 `json:"longitude" binding:"required"`
+}
+
+type CreateExpeditionRequest struct {
+	SportId           uint                     `json:"sportId" binding:"required"`
+	IsPublic          bool                     `json:"isPublic" binding:"required"`
+	Title             string                   `json:"title" binding:"required"`
+	StartDate         time.Time                `json:"startDate" binding:"required"`
+	EndDate           time.Time                `json:"endDate" binding:"required"`
+	Memo              string                   `json:"memo" binding:"required"`
+	Payments          []PaymentRequest         `json:"payments" binding:"required"`
+	Games             []GameRequest            `json:"games" binding:"required"`
+	VisitedFacilities []VisitedFacilityRequest `json:"visitedFacilities" binding:"required"`
+}

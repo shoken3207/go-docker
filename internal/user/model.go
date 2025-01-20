3c62898274ecc7822abeb2408cebb572da049406
shoken <shoken4797@gmail.com>
2025-01-20 15:20:30 +0900

    usernameのリクエスト最小文字数を修正

--- internal/user/model.go
+++ internal/user/model.go
@@ -17,7 +17,7 @@ type UpdateUserRequestBody struct {
 }
 
 type IsUniqueUsernameRequest struct {
-	Username string `uri:"username" binding:"required,min=5,max=255" example:"user123"`
+	Username string `uri:"username" binding:"required,min=1,max=255" example:"user123"`
 }
 
 // レスポンス
